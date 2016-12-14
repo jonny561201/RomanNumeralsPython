ec9aa56414a92e905f3b25de92ae122bfa0321a0
Jonathon Graf <jonathongraf@Totoro.local>
2016-12-14 11:49:21 -0600

    Updated to pass all tests by going through the dictionary in reverse order

--- RomanNumeral/RomanNumeral.py
+++ RomanNumeral/RomanNumeral.py
@@ -1,13 +1,13 @@
 class RomanNumeral(object):
     unique_roman_characters = {
-        5: "V",
-        4: "IV",
         1: "I",
+        4: "IV",
+        5: "V",
     }
 
     def convert(self, arabic_numeral):
         response = ""
-        for value, char in self.unique_roman_characters.iteritems():
+        for value, char in sorted(self.unique_roman_characters.iteritems(), reverse=True):
             if value <= arabic_numeral:
                 while arabic_numeral >= value:
                     arabic_numeral -= value
