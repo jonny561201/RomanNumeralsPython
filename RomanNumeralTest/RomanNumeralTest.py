440cd9885ea520fee11aac5cad8bfc2d21f10aeb
Jonathon Graf <jonathongraf@Totoro.local>
2016-12-16 09:00:25 -0600

    Refactored test names to better align with standard python conventions

--- RomanNumeralTest/RomanNumeralTest.py
+++ RomanNumeralTest/RomanNumeralTest.py
@@ -7,27 +7,27 @@ class RomanNumeralTest(unittest.TestCase):
     def setUp(self):
         self.romanNumeral = RomanNumeral()
 
-    def test_convertReturnsIWhenProvidedOne(self):
+    def test_convert__returns_i_when_provided_one(self):
         actual = self.romanNumeral.convert(1)
         self.assertEqual("I", actual)
 
-    def test_ConvertReturnsIIWhenProvidedTwo(self):
+    def test_convert__returns_ii_when_provided_two(self):
         actual = self.romanNumeral.convert(2)
         self.assertEqual("II", actual)
 
-    def test_ConvertReturnsIIIWhenProvidedThree(self):
+    def test_convert__returns_iii_when_provided_three(self):
         actual = self.romanNumeral.convert(3)
         self.assertEqual("III", actual)
 
-    def test_ConvertReturnsIVWhenProvidedFour(self):
+    def test_convert__returns_iv_when_provided_four(self):
         actual = self.romanNumeral.convert(4)
         self.assertEqual("IV", actual)
 
-    def test_ConvertReturnsVWhenProvidedFive(self):
+    def test_convert__returns_v_when_provided_five(self):
         actual = self.romanNumeral.convert(5)
         self.assertEqual("V", actual)
 
-    def test_ConvertReturnsVIWhenProvidedSix(self):
+    def test_convert__returns_vi_when_provided_six(self):
         actual = self.romanNumeral.convert(6)
         self.assertEqual("VI", actual)
 
@@ -38,3 +38,5 @@ class RomanNumeralTest(unittest.TestCase):
     def test_convert__returns_x_when_provided_ten(self):
         actual = self.romanNumeral.convert(10)
         self.assertEqual("X", actual)
+
+
